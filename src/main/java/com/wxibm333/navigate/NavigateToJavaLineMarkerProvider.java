b06e106e6e145ccfbb3c8f16aa5291d49b7e5b1f
wangxin <929592332@qq.com>
2020-05-29 12:53:15 +0800

    fix: LineMarker is only suitable for leaf element

--- src/main/java/com/wxibm333/navigate/NavigateToJavaLineMarkerProvider.java
+++ src/main/java/com/wxibm333/navigate/NavigateToJavaLineMarkerProvider.java
@@ -1,31 +1,21 @@
 package com.wxibm333.navigate;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
-import com.intellij.codeInspection.ex.Tools;
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiNameIdentifierOwner;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.spring.web.mvc.jam.RequestMethod;
 import com.intellij.spring.web.mvc.mapping.UrlMapping;
 import com.intellij.spring.web.mvc.model.mappings.UrlMappingPsiBasedElement;
-import com.intellij.spring.web.mvc.services.SpringMvcService;
 import com.intellij.spring.web.mvc.services.SpringMvcUtils;
 import com.intellij.ws.http.request.psi.HttpPathAbsolute;
 import com.intellij.ws.http.request.psi.HttpRequest;
-import com.intellij.xml.util.PsiElementPointer;
 import com.wxibm333.service.RestService;
 import com.wxibm333.util.ToolUtil;
-import icons.RestClientIcons;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -76,7 +66,10 @@ public class NavigateToJavaLineMarkerProvider extends RelatedItemLineMarkerProvi
                 .setAlignment(Alignment.LEFT)
                 .setTargets(psiElementList)
                 .setTooltipTitle("Navigation to target in java file");
-        result.add(builder.createLineMarkerInfo(element));
+        LeafPsiElement leafPsiElement = PsiTreeUtil.getChildOfType(element, LeafPsiElement.class);
+        if(leafPsiElement != null){
+          result.add(builder.createLineMarkerInfo(leafPsiElement));
+        }
       }
     }
   }
